d418cb3d4bb756781995143ff2890669c2d19fd9
mik3ymikes <mikey.henleys@gmail.com>
2024-02-08 14:52:16 -0600

    still bug

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+
+
+    RSpec.describe User, type: :model do
+        context 'with valid attributes' do
+          it 'is valid' do
+            user= User.new (name: "mike"  email: 'john@hotmail.com')
+            expect(user).to be_valid
+          end
+        end
+      
+        context 'with invalid attributes' do
+          it 'is invalid without a email' do
+            user = User.new(name: 'mike')
+            expect(user).to be_invalid
+          end
+      
+          it 'is invalid without a name' do
+              user = User.new(email: 'john@hotmail.com')
+              expect(user).to be_invalid
+            end
+        end
 
 end
