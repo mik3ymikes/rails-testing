109e7223b672a94af0cd97479348e8794c3b4561
mik3ymikes <mikey.henleys@gmail.com>
2024-02-07 21:31:16 -0600

    on practice problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,53 @@
- class PostsController < ApplicationController
-        def create
-          post = Post.new(post_params)
-          if post.save
-            render json: post, status: :created
-          else
-            render json: post.errors, status: :unprocessable_entity
-          end
-        end
-      
-        private
-      
-        def post_params
-          params.require(:post).permit(:title, :content)
-        end
-      end
+# app/controllers/posts_controller.rb
 
+class PostsController < ApplicationController
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post, status: :created
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index 
+    render json: Post.all
+  end
+
+  def show 
+    post = Post.find(params[:id])
+
+    if post 
+      render json: post, status: :ok 
+    else 
+      render json: {message: 'not found'}, status: :not_found
+    end
+  end
+
+  def update 
+    post = Post.find(params[:id])
+
+    if post.update(post_params)
+      render json: post, satus: :ok 
+    else 
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+
+    if post.destroy
+      # return a response with only headers and no body
+      head :no_content
+    else 
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -28,7 +28,7 @@ require 'rails_helper'
         expect(post.content).to eq('Content of the post')
       end
     end
-  end
+  end 
 
   describe 'GET /posts' do
     let!(:posts) { create_list(:post, 10) } # creating 10 posts using Factory Bot
@@ -49,14 +49,61 @@ require 'rails_helper'
     end
   end
 
+  describe 'GET /posts/:id' do
+    let!(:post) { create(:post) }
+
+    before { get "/posts/#{post.id}" }
+
+    it 'returns the post' do
+      expect(response).to have_http_status(200)
+      expect(json).not_to be_empty
+      expect(json['id']).to eq(post.id)
+    end
+
+    it 'should have the correct structure' do 
+      expect(json).to include('title', 'content')
+    end 
+    
+  end
+
+  describe 'PUT /posts/:id' do
+    let!(:post) { create(:post)} 
+    
+    context 'when the request is valid' do 
+      before { put "/posts/#{post.id}", params: { post: { title: 'My updated post', content: 'Updated content' } } }
+
+      it 'updates the post' do 
+        expect(response).to have_http_status(200)
+        expect(json['title']).to eq('My updated post')
+        expect(json['content']).to eq('Updated content')
+      end
+    end
+
+    context 'when the request is invalid' do 
+      before { put "/posts/#{post.id}", params: { post: { title: '', content: '' } } }
+
+      it 'returns status code 422' do 
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do 
+        expect(json['title']).to include("can't be blank")
+        expect(json['content']).to include("can't be blank")
+      end
+    end
+  end
+
+  describe 'DELETE /posts/:id' do
+    let!(:post) { create(:post) }
+
+    before { delete "/posts/#{post.id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+
   def json
     JSON.parse(response.body)
   end
 end
-describe 'GET /posts' do
-
-    .
-    .
-    .
-
-  end
